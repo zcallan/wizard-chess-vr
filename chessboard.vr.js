c9bb0b146f4948d5e940812523d3cde5668e4a58
zcallan <kaln.delbridge@hotmail.com>
2017-05-06 23:40:44 +1000

    began adding pieces

--- chessboard.vr.js
+++ chessboard.vr.js
@@ -19,10 +19,64 @@ const BOARD_X = -( CELL_SIZE * GRID_SIZE / 2 );
 const BOARD_Y = GRID_SIZE + METRES_FROM_GRID;
 const BOARD_Z = 0;
 
+
 class Chessboard extends Component {
   state = {
     board: initialBoard,
     turn: 'white',
+  }
+
+  movePiece( piece, to ) {
+    console.log( piece, to, this.state.board );
+
+  }
+
+  renderCell( i, j ) {
+    const cellName = String.fromCharCode( 65 + j ) + ( GRID_SIZE - i );
+
+    return (
+      <View style={{
+        backgroundColor: (( j + i ) % 2 ) ? '#FFF' : '#AAA',
+        width: CELL_SIZE,
+        height: CELL_SIZE,
+      }}>
+        <Text style={{
+          color: '#000',
+          fontSize: 0.2,
+        }}
+        >{cellName}</Text>
+      </View>
+    )
+  }
+
+  renderPieces() {
+    const { board } = this.state;
+
+    return (
+      <View style={{
+        position: 'absolute',
+        transform: [
+          { rotateX: -90 },
+          { translate: [BOARD_X, BOARD_Y, BOARD_Z] },
+        ]
+      }}>
+        {board.map( row => (
+          <View style={{ flexDirection: 'row' }}>
+            {( row && row.length > 0 ) && row.map( piece => piece && (
+              <View style={{
+                width: CELL_SIZE,
+                height: CELL_SIZE,
+              }}>
+                <Text style={{
+                  fontSize: 0.5,
+                  color: '#000',
+                }}>{piece.type}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+    )
   }
 
   render() {
@@ -38,6 +92,7 @@ class Chessboard extends Component {
         }}>{( turn === 'white' ) ? 'Your turn!' : 'Please wait for your turn.'}</Text>
 
         <View style={{
+          position: 'absolute',
           transform: [
             { rotateX: -90 },
             { translate: [BOARD_X, BOARD_Y, BOARD_Z] },
@@ -45,22 +100,12 @@ class Chessboard extends Component {
         }}>
           {_.times( GRID_SIZE, i => (
             <View style={{ flexDirection: 'row' }}>
-              {_.times( GRID_SIZE, j => (
-                <View style={{
-                  backgroundColor: (( j + i ) % 2 ) ? '#FFF' : '#AAA',
-                  width: CELL_SIZE,
-                  height: CELL_SIZE,
-                }}>
-                  <Text style={{
-                    color: '#000',
-                    fontSize: 0.2,
-                  }}
-                  >{String.fromCharCode( 65 + j ) + ( GRID_SIZE - i )}</Text>
-                </View>
-              ))}
+              {_.times( GRID_SIZE, j => this.renderCell( i, j ))}
             </View>
           ))}
         </View>
+
+        {this.renderPieces()}
       </View>
     );
   }
