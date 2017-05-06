a8f5e0070396f1dd50a76413eaa1073386579985
zcallan <kaln.delbridge@hotmail.com>
2017-05-06 19:52:06 +1000

    added a checkboard

--- index.vr.js
+++ index.vr.js
@@ -7,29 +7,44 @@ import {
   Text,
   View,
 } from 'react-vr';
+import _ from 'lodash';
 
-export default class wizardchess extends React.Component {
+const GRID_SIZE = 8;
+const CELL_SIZE = 2;
+
+export default class WizardChess extends React.Component {
   render() {
+    const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+      <View style={{
+        transform: [
+          { rotateX: -90 }
+        ]
+      }}>
+        {_.times( GRID_SIZE, i => (
+          <View style={{ flexDirection: 'row' }}>
+            {_.times( GRID_SIZE, j => (
+              <View style={{
+                backgroundColor: (( j + i ) % 2 ) ? '#FFF' : '#AAA',
+                transform: [
+                  { translate: [-(CELL_SIZE * GRID_SIZE / 2), GRID_SIZE, 1] },
+                ],
+                width: CELL_SIZE,
+                height: CELL_SIZE,
+              }}>
+                <Text style={{
+                  color: '#000',
+                  fontSize: 0.2,
+                }}
+                >{String.fromCharCode( 65 + j ) + ( GRID_SIZE - i )}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
       </View>
     );
   }
 };
 
-AppRegistry.registerComponent('wizardchess', () => wizardchess);
+AppRegistry.registerComponent('wizardchess', () => WizardChess);
